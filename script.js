874af18cc6184d5c2d966f0c03f7c6e78d7d422a
jogindrakumar <joginder9868@outlook.com>
2024-01-31 21:39:35 +0530

    timer

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 gsap.from(".line h1",{
     y:150,
-    stragger:0.5,
+    stagger:0.25,
     duration:0.6,
     delay:0.5
 })
+
+var h5timer = document.querySelector("#line1-part1 h5")
+var grow = 0
+setInterval(function() {
+  if(grow < 100){
+   
+   h5timer.innerHTML =  grow++;
+  }
+  else{
+    h5timer.innerHTML =  grow;
+  }
+    
+},35)
